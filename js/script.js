25ed6ff2454451be986da0396e1cead249ce0d24
Neil Chen <neilwchen@gmail.com>
2017-12-06 01:47:03 -0500

    Replace bootstrap with skeleton

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-$(document).foundation()
+$(function() {
+
+    var context = {
+        people: [
+            { name: "foo" },
+            { name: "bar" },
+            { name: "baz" }
+        ]
+    };
+
+    var template_script = $("#template-script").html();
+    var template = Handlebars.compile(template_script);
+    var compiled_html = template(context);
+    $("#navbar-script").html(compiled_html);
+
+});
