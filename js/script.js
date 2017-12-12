8e7eab313d95f3f306eb55ce11041b5e505456f9
Neil Chen <neilwchen@gmail.com>
2017-12-11 23:49:13 -0500

    Remove extra js files, add navbar generation to script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-$(function() {
+// Should probably use a templating engine here, or server-side includes, or $.load()
+// But this is easier to read, and should only ever get called once per page load
+const load_navbar = function() {
+    var nav_people = ['Will', 'Chris', 'Erinn', 'Isaiah', 'Kendal', 'James', 'Lalitha', 'Iliana', 'Alice', 'Yefri', 'Amy'];
+    var nav_contents = '<div class="row">';
+    var post_html = '</a></div>';
 
-    var context = {
-        people: [
-            { name: "foo" },
-            { name: "bar" },
-            { name: "baz" }
-        ]
-    };
+    for(var i=0; i<nav_people.length; i++) {
+        var pre_html = '<div class="column"><a href="#' + nav_people[i] + '">';
+        nav_contents = nav_contents + pre_html + nav_people[i] + post_html;
+    }
 
-    var template_script = $("#template-script").html();
-    var template = Handlebars.compile(template_script);
-    var compiled_html = template(context);
-    $("#navbar-script").html(compiled_html);
+    nav_contents = nav_contents + '</div>';
+    $('#nav-target').html(nav_contents);
+};
 
+$(document).ready(function() {
+    load_navbar();
 });
